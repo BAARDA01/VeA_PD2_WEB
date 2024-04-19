9dcd3f13e25bda3e8e7b48020aaf2d20761c8a43
Kārlis Bērziņš <151729535+BAARDA01@users.noreply.github.com>
2024-04-19 17:03:54 +0300

    ok

--- src/resources/views/author/list.blade.php
+++ src/resources/views/author/list.blade.php
@@ -1,3 +1,36 @@
-<div>
-    <!-- I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison -->
-</div>
+@extends('layout')
+ 
+@section('content')
+ 
+    <h1>{{ $title }}</h1>
+ 
+    @if (count($items) > 0)
+ 
+        <table class="table table-striped table-hover table-sm">
+            <thead class="thead-light">
+                <tr>
+                    <th>ID</td>
+                    <th>Vārds</td>
+                    <th>&nbsp;</td>
+                </tr>
+            </thead>
+            <tbody>
+ 
+            @foreach($items as $author)
+            <tr>
+                <td>{{ $author->id }}</td>
+                <td>{{ $author->name }}</td>
+                <td>Labot / Dzēst</td>
+            </tr>
+            @endforeach
+ 
+            </tbody>
+        </table>
+ 
+    @else
+ 
+        <p>Nav atrasts neviens ieraksts</p>
+ 
+    @endif
+ 
+@endsection
